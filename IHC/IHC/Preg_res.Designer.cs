104be2612d3c0158241c7e2cda6adf43df6ca279
alejandro aguirre kuoman <alejandro.aguirre.k@outlook.com>
2013-06-23 21:35:50 +0200

    Version 1.0.0 estable 1

--- IHC/IHC/Preg_res.Designer.cs
+++ IHC/IHC/Preg_res.Designer.cs
@@ -28,38 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pregunta = new System.Windows.Forms.Label();
+            this.preg = new System.Windows.Forms.Label();
             this.RDrespuesta2 = new System.Windows.Forms.RadioButton();
             this.RDrespuesta1 = new System.Windows.Forms.RadioButton();
-            this.RDrespuesta3 = new System.Windows.Forms.RadioButton();
-            this.RDrespuesta4 = new System.Windows.Forms.RadioButton();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.final = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pregunta
+            // preg
             // 
-            this.pregunta.AutoSize = true;
-            this.pregunta.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pregunta.ForeColor = System.Drawing.Color.White;
-            this.pregunta.Location = new System.Drawing.Point(71, 71);
-            this.pregunta.Name = "pregunta";
-            this.pregunta.Size = new System.Drawing.Size(52, 21);
-            this.pregunta.TabIndex = 0;
-            this.pregunta.Text = "label1";
+            this.preg.AutoSize = true;
+            this.preg.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.preg.ForeColor = System.Drawing.Color.White;
+            this.preg.Location = new System.Drawing.Point(71, 71);
+            this.preg.Name = "preg";
+            this.preg.Size = new System.Drawing.Size(42, 21);
+            this.preg.TabIndex = 0;
+            this.preg.Text = "preg";
             // 
             // RDrespuesta2
             // 
             this.RDrespuesta2.AutoSize = true;
             this.RDrespuesta2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RDrespuesta2.Location = new System.Drawing.Point(75, 170);
+            this.RDrespuesta2.Location = new System.Drawing.Point(96, 181);
             this.RDrespuesta2.Name = "RDrespuesta2";
-            this.RDrespuesta2.Size = new System.Drawing.Size(119, 25);
+            this.RDrespuesta2.Size = new System.Drawing.Size(46, 25);
             this.RDrespuesta2.TabIndex = 1;
             this.RDrespuesta2.TabStop = true;
-            this.RDrespuesta2.Text = "radioButton1";
+            this.RDrespuesta2.Text = "no";
             this.RDrespuesta2.UseVisualStyleBackColor = true;
             this.RDrespuesta2.CheckedChanged += new System.EventHandler(this.RDrespuesta2_CheckedChanged_1);
             // 
@@ -67,45 +71,19 @@
             // 
             this.RDrespuesta1.AutoSize = true;
             this.RDrespuesta1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RDrespuesta1.Location = new System.Drawing.Point(75, 136);
+            this.RDrespuesta1.Location = new System.Drawing.Point(96, 147);
             this.RDrespuesta1.Name = "RDrespuesta1";
-            this.RDrespuesta1.Size = new System.Drawing.Size(119, 25);
+            this.RDrespuesta1.Size = new System.Drawing.Size(39, 25);
             this.RDrespuesta1.TabIndex = 2;
             this.RDrespuesta1.TabStop = true;
-            this.RDrespuesta1.Text = "radioButton2";
+            this.RDrespuesta1.Text = "si";
             this.RDrespuesta1.UseVisualStyleBackColor = true;
             this.RDrespuesta1.CheckedChanged += new System.EventHandler(this.RDrespuesta2_CheckedChanged);
-            // 
-            // RDrespuesta3
-            // 
-            this.RDrespuesta3.AutoSize = true;
-            this.RDrespuesta3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RDrespuesta3.Location = new System.Drawing.Point(75, 205);
-            this.RDrespuesta3.Name = "RDrespuesta3";
-            this.RDrespuesta3.Size = new System.Drawing.Size(119, 25);
-            this.RDrespuesta3.TabIndex = 3;
-            this.RDrespuesta3.TabStop = true;
-            this.RDrespuesta3.Text = "radioButton3";
-            this.RDrespuesta3.UseVisualStyleBackColor = true;
-            this.RDrespuesta3.CheckedChanged += new System.EventHandler(this.RDrespuesta3_CheckedChanged);
-            // 
-            // RDrespuesta4
-            // 
-            this.RDrespuesta4.AutoSize = true;
-            this.RDrespuesta4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RDrespuesta4.Location = new System.Drawing.Point(75, 238);
-            this.RDrespuesta4.Name = "RDrespuesta4";
-            this.RDrespuesta4.Size = new System.Drawing.Size(119, 25);
-            this.RDrespuesta4.TabIndex = 4;
-            this.RDrespuesta4.TabStop = true;
-            this.RDrespuesta4.Text = "radioButton4";
-            this.RDrespuesta4.UseVisualStyleBackColor = true;
-            this.RDrespuesta4.CheckedChanged += new System.EventHandler(this.RDrespuesta4_CheckedChanged);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(289, 306);
+            this.button2.Location = new System.Drawing.Point(1035, 243);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(156, 23);
             this.button2.TabIndex = 6;
@@ -127,43 +105,88 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panel1.Controls.Add(this.pregunta);
+            this.panel1.Controls.Add(this.preg);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(536, 115);
+            this.panel1.Size = new System.Drawing.Size(1208, 115);
             this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.final);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(4, 287);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1203, 278);
+            this.panel2.TabIndex = 9;
+            // 
+            // final
+            // 
+            this.final.AutoSize = true;
+            this.final.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.final.ForeColor = System.Drawing.Color.White;
+            this.final.Location = new System.Drawing.Point(63, 65);
+            this.final.Name = "final";
+            this.final.Size = new System.Drawing.Size(52, 21);
+            this.final.TabIndex = 1;
+            this.final.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(33, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Resultado:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.panel1);
+            this.panel3.Controls.Add(this.RDrespuesta2);
+            this.panel3.Controls.Add(this.RDrespuesta1);
+            this.panel3.Location = new System.Drawing.Point(4, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1203, 281);
+            this.panel3.TabIndex = 10;
             // 
             // Preg_res
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(535, 373);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.RDrespuesta4);
-            this.Controls.Add(this.RDrespuesta3);
-            this.Controls.Add(this.RDrespuesta1);
-            this.Controls.Add(this.RDrespuesta2);
+            this.ClientSize = new System.Drawing.Size(1207, 282);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Name = "Preg_res";
             this.Text = "Preg_res";
+            this.Load += new System.EventHandler(this.Preg_res_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label pregunta;
+        private System.Windows.Forms.Label preg;
         private System.Windows.Forms.RadioButton RDrespuesta2;
         private System.Windows.Forms.RadioButton RDrespuesta1;
-        private System.Windows.Forms.RadioButton RDrespuesta3;
-        private System.Windows.Forms.RadioButton RDrespuesta4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label final;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- IHC/IHC/Preg_res.cs
+++ IHC/IHC/Preg_res.cs
@@ -14,10 +14,27 @@ namespace IHC
     public partial class Preg_res : Form
     {
         ArrayList preguntas = new ArrayList();
+        ArrayList sino = new ArrayList();
+        ArrayList resp = new ArrayList();
+        ArrayList comentario = new ArrayList();
+        ArrayList conjunto = new ArrayList();
+        string radios = "";
+        int cont = 0;
         public Preg_res()
         {
             InitializeComponent();
-            
+
+            preguntas.Add( "Ponga Mucha énfasis en lo siguiente: \n mire fijamente su texto por 15 segundos, se vé mas borroso el texto?");
+            sino.Add("si");
+            resp.Add("deveria cambiar su color a uno con menos azul en su composición");
+            comentario.Add("trate de usar colores e iconos estandarizados");
+
+
+
+            preguntas.Add( "si su aplicación esta dirigida a varios paises: \n Ud. A investigado sobre lo que significa sus iconos y textos en dichos paises?");
+            sino.Add("no");
+            resp.Add( "Deveria investigar lo antes dicho debido a que en alguno de esos países puede ser tomado como algo impropio según su cultura");
+            comentario.Add("trate de utilizar iconos comunes en todo el mundo");
         }
 
         private void RDrespuesta2_CheckedChanged(object sender, EventArgs e)
@@ -25,8 +42,7 @@ namespace IHC
             if (RDrespuesta1.Checked == true)
             {
                 RDrespuesta2.Checked = false;
-                RDrespuesta3.Checked = false;
-                RDrespuesta4.Checked = false;
+                radios = "si";
             }
         }
 
@@ -35,34 +51,51 @@ namespace IHC
             if (RDrespuesta2.Checked == true)
             {
                 RDrespuesta1.Checked = false;
-                RDrespuesta3.Checked = false;
-                RDrespuesta4.Checked = false;
+                radios = "no";
             }
         }
 
         private void RDrespuesta3_CheckedChanged(object sender, EventArgs e)
         {
-            if (RDrespuesta3.Checked == true)
-            {
-                RDrespuesta2.Checked = false;
-                RDrespuesta1.Checked = false;
-                RDrespuesta4.Checked = false;
-            }
+            
         }
 
         private void RDrespuesta4_CheckedChanged(object sender, EventArgs e)
         {
-            if (RDrespuesta4.Checked == true)
-            {
-                RDrespuesta2.Checked = false;
-                RDrespuesta3.Checked = false;
-                RDrespuesta1.Checked = false;
-            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (sino[cont].ToString() == radios)
+            {
+                conjunto.Add(cont);
+            }
+            cont++;
+            if (cont == 1)
+            {
+                preg.Text = preguntas[cont].ToString();
+            }
+            else {
+                panel1.Hide();
+                panel3.Hide();
+                panel2.Show();
+                panel2.Location = new System.Drawing.Point(4,0);
+                final.Text = "";
+                foreach (int conte in conjunto)
+                {
+                    final.Text += resp[conte].ToString();
+                    final.Text += "\n";
+                }
+            }
+            
+        }
 
+        private void Preg_res_Load(object sender, EventArgs e)
+        {
+            preg.Text = preguntas[cont].ToString();
+            panel1.Show();
+            panel3.Show();
+            panel2.Hide();
         }
     }
 }
